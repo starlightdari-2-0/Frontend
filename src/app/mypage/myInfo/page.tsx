f0fd2845ed269a89a571324e03abe05c7d9bb098
jjoo1229 <juyeong@sookmyung.ac.kr>
2025-09-11 18:24:09 +0900

    feat: 마이페이지 UI 제작완료 #4

--- src/app/mypage/myInfo/page.tsx
+++ src/app/mypage/myInfo/page.tsx
@@ -1,31 +1,55 @@
 "use client";
 
 import React from "react";
-import styled from "styled-components";
-import Header from "../../../components/header";
+import Header from "../../../components/myPageHeader";
 import UserInfo from "../../../components/userInfo";
-import MyStar from "../../../components/myStar";
+import ConstellationList from "../../../components/myConstellationList";
+import ActivityMenu from "../../../components/myActivityMenu";
+import NavBar from "../../../components/navBar";
+import Image from "next/image";
+import arrow from "/public/arrow_right.svg";
+import { Arrow, Container, Section, SectionHeader, SectionTitle } from "./styles";
+
 
 export default function Page() {
+  // mock data
+  const constellations = [
+    {
+      id: 1,
+      name: "후추 자리",
+      count: 5,
+      imageUrl: "https://place-puppy.com/80x80",
+    },
+    {
+      id: 2,
+      name: "초코 자리",
+      count: 3,
+      imageUrl: "https://placekitten.com/80/80",
+    },
+  ];
+
+
   return (
-    <>
+    <Container>
       <Header />
-      <Body>
-        <UserInfo />
-        <MyStar />
-      </Body>
-    </>
+      <UserInfo />
+      <Section>
+        <SectionHeader>
+          <SectionTitle>나의 활동</SectionTitle>
+        </SectionHeader>
+        <ActivityMenu />
+      </Section>
+
+      <Section>
+        <SectionHeader>
+          <SectionTitle>나의 별자리</SectionTitle>
+          <Arrow><Image src={arrow} alt="move to constellation page" /></Arrow>
+        </SectionHeader>
+        <ConstellationList constellations={constellations} />
+      </Section>
+
+      <NavBar />
+    </Container>
   );
 }
 
-const Body = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100vw;
-  align-items: center;
-  position: absolute;
-  top: 90px;
-  color: #fff;
-  padding-top: 30px;
-  gap: 65px;
-`;
---
+++ src/app/mypage/myInfo/styles.ts
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 0 auto;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  width: 360px;
+  min-height: 100vh;
+  min-width: 360px;
+  max-width: 767px;
+  opacity: 1;
+  padding-right: 16px;
+  padding-bottom: 60px;
+  padding-left: 16px;
+  background-color: #1F2027;
+`;
+
+export const Section = styled.div`
+  padding-bottom: 16px;
+`;
+
+export const SectionTitle = styled.h2`
+  font-family: Pretendard;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 150%;
+  color: #D9E0ED;
+`;
+
+export const SectionHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px 0;
+`;
+
+export const Arrow = styled.button`
+  border: none;
+  background: none;
+  cursor: pointer;
+`;
