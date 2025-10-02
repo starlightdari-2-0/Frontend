ed1805fe09dd11bc6675a27b5cf4321fc93c9aff
jjoo1229 <juyeong@sookmyung.ac.kr>
2025-10-02 23:02:08 +0900

    feat: 내비게이션 바 UI 제작 완료

--- src/components/navBar/styles.ts
+++ src/components/navBar/styles.ts
@@ -1,119 +1,31 @@
 import styled from "styled-components";
-import Image from "next/image";
 
-export const Wrapper = styled.div<{ isOpen: boolean }>`
+export const Nav = styled.div`
   display: flex;
-  flex-direction: column;
-  background: #0c1322;
-  color: #fff;
-  border-radius: 7px 0 0 7px;
-  position: fixed;
-  top: 0;
-  right: ${({ isOpen }) => (isOpen ? "0" : "-282px")};
-  width: 250px;
-  height: 100vh;
-  padding: 16px;
-  transition: right 0.3s ease-in-out;
-  box-shadow: ${({ isOpen }) =>
-        isOpen ? "-4px 0 10px rgba(0, 0, 0, 0.2)" : "none"};
-  z-index: 1000;
-`;
-
-export const Profile = styled.div`
-  display: flex;
+  justify-content: space-around;
   align-items: center;
-  gap: 10px;
-  padding-bottom: 20px;
-  position: relative;
-`;
-
-export const ProfilePhoto = styled(Image)`
-  width: 40px;
+  background: #2A2F39;
+  padding: 10px 0;
+  width: 280px;
   height: 40px;
-  border-radius: 100px;
-  background: gray;
+  border-radius: 999px;
+  opacity: 1;
+  position: absolute; 
+  bottom: 40px; 
+  left: 50%; 
+  transform: translateX(-50%);
 `;
 
-export const User = styled.div`
-  display: flex;
-  align-items: baseline;
-  gap: 7px;
-  font-size: 13px;
-`;
-
-export const UserName = styled.span`
-  font-weight: 900;
-  font-size: 17px;
-`;
-
-export const MenuBar = styled.div`
+export const NavItem = styled.div<{ active?: boolean }>`
   display: flex;
   flex-direction: column;
-`;
-
-export const Menu = styled.div`
-  display: flex;
   align-items: center;
-  padding: 20px 0 20px 10px;
-  font-weight: 900;
-  font-size: 20px;
+  justify-content: center;
+  font-size: 12px;
+  width: 80px;
+  height: 60px;
+  gap: 4px;
+  opacity: 1;
   cursor: pointer;
-  gap: 10px;
-`;
-
-export const SubMenu = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
-`;
-
-export const PetImage = styled(Image)`
-  width: 40px;
-  height: 40px;
-  border-radius: 100px;
-`;
-
-export const Item = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 15px;
-  padding: 10px;
-  cursor: pointer;
-`;
-
-export const PetItem = styled(Item)`
-  background-color: #162132;
-  cursor: pointer;
-`;
-
-export const ToggleButton = styled.button`
-  display: flex;
-  align-items: center;
-  border: none;
-  background: none;
-  cursor: pointer;
-`;
-
-export const AlertBadge = styled.span``;
-
-export const NoPet = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
-  padding: 30px 10px;
-  height: 80px;
-  align-items: center;
-  // justify-content: center;
-  position: relative;
-`;
-
-export const Button = styled.button`
-  border: none;
-  background: #22225e;
-  cursor: pointer;
-  padding: 10px 20px;
-  color: #fff;
-  border-radius: 5px;
-  position: absolute;
-  bottom: 10px;
+  color: ${({ active }) => (active ? "white" : "#888")};
 `;
