a7328374f45a0af4171f8117ef6930d853e82cb1
jjoo1229 <juyeong@sookmyung.ac.kr>
2026-02-15 17:04:33 +0900

    fix: mock data 삭제 후 재연결 #20

--- src/app/myPost/page.tsx
+++ src/app/myPost/page.tsx
@@ -1,52 +1,43 @@
 "use client";
 
 import React from "react";
-import { PostType } from "../../types/postType";
 import Header from "../../components/header";
 import { Post } from "../../components/post";
 import { Container } from "./styles";
-
-const mockPost: PostType = {
-    id: 1,
-    author: "닉네임",
-    title: "제목제목",
-    content: "내용이 길어지면 스크롤되는 그런 방식...",
-    imageUrl: "/maru.svg",
-    date: "2025.08.25",
-    likes: {
-        like1: 1,
-        like2: 0,
-        like3: 0,
-    },
-    comments: [
-        {
-            id: 1,
-            author: "닉네임",
-            content: "댓글 내용",
-            date: "2025.08.25",
-            replies: [
-                {
-                    id: 2,
-                    author: "닉네임",
-                    content: "대댓글 내용",
-                    date: "2025.08.25",
-                },
-                {
-                    id: 3,
-                    author: "닉네임",
-                    content: "대댓글 달았어요",
-                    date: "2025.08.26",
-                },
-            ],
-        },
-    ],
-};
+import { useSelectMemoryStarByMemId } from "../../api/generated/memory-star-controller/memory-star-controller";
+import { useParams } from "next/navigation";
+import { MemoryStarRepDto } from "../../api/generated/model";
 
 const MyPost = () => {
+    const params = useParams();
+    const memoryId = Number(params?.id);
+    const { data: postData, isLoading, isError, error } = useSelectMemoryStarByMemId(memoryId);
+
+    if (isLoading) {
+        return (
+            <Container>
+                <Header title="내 추억글" backUrl="/" />
+                <div style={{ padding: "20px", textAlign: "center" }}>추억을 불러오는 중입니다...</div>
+            </Container>
+        );
+    }
+
+    if (isError || !postData) {
+        return (
+            <Container>
+                <Header title="내 추억글" backUrl="/" />
+                <div style={{ padding: "20px", textAlign: "center" }}>
+                    추억을 불러오지 못했습니다. <br />
+                    {error instanceof Error ? error.message : "알 수 없는 오류가 발생했습니다."}
+                </div>
+            </Container>
+        );
+    }
+
     return (
         <Container>
             <Header title="내 추억글" backUrl="" />
-            <Post post={mockPost} />
+            {postData && <Post post={postData as MemoryStarRepDto} />}
         </Container>
     );
 }
